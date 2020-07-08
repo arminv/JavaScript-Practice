e259a2e9d65c229058f0678038792be3129db9a1
arminv <arminv@users.noreply.github.com>
2020-07-08 00:53:55 -0400

    Added comments

--- Section 8/Maps & Sets/206-maps.js
+++ Section 8/Maps & Sets/206-maps.js
@@ -7,8 +7,10 @@ const personData = new Map([[person1, [{ date: 'yesterday', price: 10 }]]]);
 personData.set(person2, [{ date: 'two weeks ago', price: 100 }]);
 
 console.log(personData);
+// NOTE: here the key is an Object, so we can easily pass it to the get() method of maps:
 console.log(personData.get(person1));
 
+// NOTE: we can have keys of mixed types (one is an Object, another is string, etc.)
 for (const [key, value] of personData.entries()) {
   console.log(key, value);
 }
@@ -21,4 +23,5 @@ for (const value of personData.values()) {
   console.log(value);
 }
 
+// NOTE: size() returns the number of key-value pairs:
 console.log(personData.size);
