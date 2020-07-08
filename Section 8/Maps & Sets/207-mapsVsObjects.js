eeacfb7f11277a58aa66478ff50a1d90bc249a6d
arminv <arminv@users.noreply.github.com>
2020-07-08 00:58:56 -0400

    End of 207

---
+++ Section 8/Maps & Sets/207-mapsVsObjects.js
@@ -0,0 +1,11 @@
+// NOTE:
+
+// Maps:
+// 1) Can use ANY values(and types) as keys
+// 2) Better performance for large quantities of data
+// 3) Better performance when adding or removing data frequently
+
+// Objects:
+// 1) Only may use: strings, numbers or symbols as keys
+// 2) Perfect for small/medium-sized sets of data
+// 3) Easier/quicker to create (typically also with better performance)
