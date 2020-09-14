3a7a9e3bf75ef48b2e0dccf09b8b29ed00ffbaa4
arminv <arminv@users.noreply.github.com>
2020-09-14 00:13:31 -0400

    End of 267

--- Section 11/app.js
+++ Section 11/app.js
@@ -10,7 +10,7 @@
 //   }
 // }
 
-// NOTE: we can use functions instead of constructor() function:
+// NOTE: we can use functions instead of the constructor() function - starting with uppercase is only convention:
 function Person() {
   this.age = 30;
   this.name = 'Armin';
@@ -19,5 +19,6 @@ function Person() {
   };
 }
 
+// NOTE: the 'new' keyword is important as it still creates an instance of Person - 'new' creates an Object (this={}) and then sets its properties:
 const person = new Person();
 person.greet();
