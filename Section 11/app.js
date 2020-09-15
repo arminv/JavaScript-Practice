08ff880104029dc6a64eb39afddaae1bbd08fe18
arminv <arminv@users.noreply.github.com>
2020-09-15 00:11:59 -0400

    End of 269

--- Section 11/app.js
+++ Section 11/app.js
@@ -19,6 +19,24 @@ function Person() {
   };
 }
 
+// NOTE: 'prototype' is used to assign/set a prototype to an object (i.e. to include something in '__proto__'):
+// NOTE: 'extends' automatically does this behind the scene:
+Person.prototype = {
+  printAge() {
+    // NOTE: the 'this' keyword in prototypes  ALWAYS refers to the object that called it (i.e. the instance that calls the method):
+    console.log(this.age);
+  },
+};
+
+console.dir(Person);
+
 // NOTE: the 'new' keyword is important as it still creates an instance of Person - 'new' creates an Object (this={}) and then sets its properties:
-const person = new Person();
-person.greet();
+const p = new Person();
+p.greet();
+p.printAge();
+// NOTE: prototypes are just 'fallback objects' - they are just base classes.
+// NOTE: prototypes themselves are Objects
+// NOTE: In console, '__proto__' indicates prototypes. This is present for ALL objects in JS (functions, arrays, custom objects, etc.)!
+// NOTE: In console, 'prototype' property only exists for function objects (NOT all objects)!
+console.log(p.__proto__);
+console.log(p.__proto__ === Person.prototype);
