1306497a800460820c8a575198769050aa5e6389
arminv <arminv@users.noreply.github.com>
2020-09-23 00:55:59 -0400

    End of 281

--- Section 12/assets/scripts/app.js
+++ Section 12/assets/scripts/app.js
@@ -1,11 +1,38 @@
 class Tooltip {}
 
-class ProjectItem {}
+class ProjectItem {
+  constructor(id) {
+    this.id = id;
+    this.connectSwitchButton();
+    this.connectMoreInfoButton();
+  }
+
+  connectSwitchButton() {}
+  connectMoreInfoButton() {
+    const ProjectItemElement = document.getElementById(this.id);
+    const switchBtn = ProjectItemElement.querySelector('button:last-of-type');
+    switchBtn.addEventListener('click');
+  }
+}
 
 class ProjectList {
+  projects = [];
+
   constructor(type) {
     const prjItems = document.querySelectorAll(`#${type}-projects li`);
-    console.log(prjItems);
+    for (const prjItem of prjItems) {
+      this.projects.push(new ProjectItem(prjItem.id));
+    }
+    console.log(this.projects);
+  }
+
+  addProject() {}
+
+  switchProject(projectId) {
+    // const projectIndex = this.projects.findIndex((p) => p.id === projectId);
+    // this.projects.splice(projectIndex, 1);
+    // NOTE: this is easier than above:
+    this.projects.filter((p) => p.id !== projectId);
   }
 }
 
