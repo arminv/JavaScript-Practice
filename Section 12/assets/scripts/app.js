1f7b8103f8bbe0fd5788387433dc862930c5e9cb
arminv <arminv@users.noreply.github.com>
2020-09-26 14:29:57 -0400

    End of 282

--- Section 12/assets/scripts/app.js
+++ Section 12/assets/scripts/app.js
@@ -1,8 +1,9 @@
 class Tooltip {}
 
 class ProjectItem {
-  constructor(id) {
+  constructor(id, updateProjectListsFunction) {
     this.id = id;
+    this.updateProjectListsHandler = updateProjectListsFunction;
     this.connectSwitchButton();
     this.connectMoreInfoButton();
   }
@@ -11,7 +12,7 @@ class ProjectItem {
   connectMoreInfoButton() {
     const ProjectItemElement = document.getElementById(this.id);
     const switchBtn = ProjectItemElement.querySelector('button:last-of-type');
-    switchBtn.addEventListener('click');
+    switchBtn.addEventListener('click', this.updateProjectListsHandler);
   }
 }
 
@@ -22,7 +23,9 @@ class ProjectList {
     this.type = type;
     const prjItems = document.querySelectorAll(`#${type}-projects li`);
     for (const prjItem of prjItems) {
-      this.projects.push(new ProjectItem(prjItem.id));
+      this.projects.push(
+        new ProjectItem(prjItem.id, this.switchProject.bind(this))
+      );
     }
     console.log(this.projects);
   }
@@ -31,7 +34,9 @@ class ProjectList {
     this.switchHandler = switchHandlerFunction;
   }
 
-  addProject() {}
+  addProject() {
+    console.log(this);
+  }
 
   switchProject(projectId) {
     // const projectIndex = this.projects.findIndex((p) => p.id === projectId);
@@ -46,6 +51,12 @@ class App {
   static init() {
     const activeProjectsList = new ProjectList('active');
     const finishedProjectsList = new ProjectList('finished');
+    activeProjectsList.setSwitchHandlerFunction(
+      finishedProjectsList.addProject.bind(finishedProjectsList)
+    );
+    finishedProjectsList.setSwitchHandlerFunction(
+      activeProjectsList.addProject.bind(activeProjectsList)
+    );
   }
 }
 
