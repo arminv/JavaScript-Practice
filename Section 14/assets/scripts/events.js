da9d4f6b2fcd11d8639f1eab325e60fc5cc307e1
arminv <arminv@users.noreply.github.com>
2020-10-23 20:22:11 -0400

    End of 308

--- Section 14/assets/scripts/events.js
+++ Section 14/assets/scripts/events.js
@@ -1,8 +1,10 @@
-const button = document.querySelector('button');
+const buttons = document.querySelectorAll('button');
 
 // NOTE: we could also have this inline and inside the HTML file (as an attribtue, 'onclick' on the <button >), but it's not the recommended way hence we use onclick here:
-const buttonClickHandler = () => {
-  alert('Button was clicked!');
+const buttonClickHandler = (event) => {
+  // NOTE: check MDN for more info about this returned object - here, 'target' gives the element that called the function and it has a property 'disabled':
+  event.target.disabled = true;
+  console.log('event', event);
 };
 
 const anotherButtonClickHandler = () => {
@@ -13,10 +15,16 @@ const anotherButtonClickHandler = () => {
 // button.onclick = buttonClickHandler;
 // button.onclick = anotherButtonClickHandler;
 
+const boundFn = buttonClickHandler.bind(this);
+
 // NOTE: a better way is the following:
-button.addEventListener('click', buttonClickHandler);
-setTimeout(() => {
-  // NOTE: it's important to apply removeEventListener to the exact same setup/args as when we attach the listener - it has to be the exact same function object:
-  // NOTE: for the same reason above, we cannot use .bind(this) as .bind() creates a new function object!
-  button.removeEventListener('click', buttonClickHandler);
-}, 2000);
+// button.addEventListener('click', buttonClickHandler);
+// setTimeout(() => {
+//   // NOTE: it's important to apply removeEventListener to the exact same setup/args as when we attach the listener - it has to be the exact same function object:
+//   // NOTE: for the same reason above, we cannot use .bind(this) as .bind() creates a new function object!
+//   button.removeEventListener('click', buttonClickHandler);
+// }, 2000);
+
+buttons.forEach((btn) => {
+  btn.addEventListener('click', buttonClickHandler);
+});
