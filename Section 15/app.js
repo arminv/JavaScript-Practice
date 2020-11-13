95f4b6cb4ab37272e9f73df0d121f883fc3b470d
arminv <arminv@users.noreply.github.com>
2020-11-12 21:18:24 -0500

    End of 327

--- Section 15/app.js
+++ Section 15/app.js
@@ -33,3 +33,25 @@ function printHobbies(h) {
 }
 
 printHobbies(hobbies);
+
+// NOTE: In general we prefer pure functions over normal ones as they are more predictable.
+// ---------------------------------------------------------------------
+// ---------------------------------------------------------------------
+// Factory Functions:
+function createTaxCalculator(tax) {
+  function calculateTax(amount) {
+    return amount * tax;
+  }
+
+  return calculateTax;
+}
+
+// const vatAmount = calculateTax(100, 0.19);
+// const incomeTax = calculateTax(100, 0.25);
+
+// NOTE: by using factory functions, we can instead use - this way we don't have to pass in tax rate as and arg, we only need to pass amount:
+const calculateVatAmount = createTaxCalculator(0.19);
+const calculateIncomeTaxAmount = createTaxCalculator(0.25);
+
+console.log(calculateVatAmount(100));
+console.log(calculateIncomeTaxAmount(100));
