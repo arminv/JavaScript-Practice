535095597146de761aca831ecae4b4634c491d0c
arminv <arminv@users.noreply.github.com>
2020-11-19 18:23:39 -0500

    End of 338

--- Section 16/app.js
+++ Section 16/app.js
@@ -6,3 +6,21 @@ console.log(Math.pow(2, 53) - 1);
 
 console.log(Number.MIN_SAFE_INTEGER); // Smallest integer possible
 console.log(Number.MIN_VALUE); // Smallest floating number possible
+
+// NOTE: because JS converts numbers into binary format and then back to decimal, sometimes we have issues with accuracy:
+console.log(0.2 + 0.4 === 0.6); // false
+
+// MORE INFO:
+// https://stackoverflow.com/questions/11695618/dealing-with-float-precision-in-javascript
+// https://en.wikipedia.org/wiki/Floating-point_arithmetic
+// https://2ality.com/2012/04/number-encoding.html
+
+// NOTE: we can see the binary representation of a decimal number like this:
+console.log((1).toString(2)); // 2 specifies the base (binary in this case)
+// NOTE: same way that for example 1/3 cannot be calculated exactly, some fractions cannot be precisely represented by the binary system
+console.log((1 / 5).toString(2));
+
+// NOTE: for representation purposes, toFixed() is good enough. However, for accuracy of calculations themselves,
+// we can do this 'trick' - multiply by 100 to ensure we work with 'integers' (not 100% true as all numbers are floating in JS)
+// alternatively, use 3rd party libraries to help.
+console.log(20.2 * 100);
