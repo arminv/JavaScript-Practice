51577d0875a9bd900125d4d711c4d5570daab138
arminv <arminv@users.noreply.github.com>
2020-11-18 20:23:24 -0500

    End of 337

---
+++ Section 16/app.js
@@ -0,0 +1,8 @@
+// NOTE: all numbers in JS are floating point (e.g. 3 will be saved as 3.0, etc.)
+// NOTE: there are minimum and maximum bounds for numbers in JS:
+console.log(Number.MAX_SAFE_INTEGER); // Biggest integer possible (it is 2^53 - 1)
+console.log(Number.MAX_VALUE); // Biggest floating number possible
+console.log(Math.pow(2, 53) - 1);
+
+console.log(Number.MIN_SAFE_INTEGER); // Smallest integer possible
+console.log(Number.MIN_VALUE); // Smallest floating number possible
