6e2585ad939ec071bc2502ad888a5aeb42b4591b
arminv <arminv@users.noreply.github.com>
2020-11-21 21:54:15 -0500

    End of 351

--- Section 17/app.js
+++ Section 17/app.js
@@ -5,4 +5,13 @@ function trackUserHandler() {
   console.log('Clicked!');
 }
 
+// NOTE: this is an async task:
 button.addEventListener('click', trackUserHandler);
+
+// NOTE: we see 'Clicked!' only after the below has been executed (because of the way browser handles the Event Loop)
+let result = 0;
+for (let i = 0; i < 100000000; i++) {
+  result += i;
+}
+
+console.log(result);
