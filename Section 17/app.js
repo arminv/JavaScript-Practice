6fa7cfc27956801dc59f3269a7b1328fde536957
arminv <arminv@users.noreply.github.com>
2020-11-26 19:23:35 -0500

    End of 354

--- Section 17/app.js
+++ Section 17/app.js
@@ -3,14 +3,29 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
+const setTimer = (duration) => {
+  // NOTE: the function inside Promise() will be executed when the promise is created:
+  const promise = new Promise((resolve, reject) => {
+    // NOTE: we are essentially 'promisifying a built-in API' (setTimeout() in this case) here:
+    setTimeout(() => {
+      // NOTE: the browser passes 'resolve' and 'reject' to the promise by default:
+      resolve('DONE!');
+    }, duration);
+  });
+
+  return promise;
+};
+
 function trackUserHandler() {
   console.log('Clicked!');
   navigator.geolocation.getCurrentPosition(
     (posData) => {
+      // NOTE: the function inside then() executes whenever the promise is resolved:
+      setTimer(2000).then((data) => console.log(data, posData));
       // NOTE: we CAN nest async operations inside each other:
-      setTimeout(() => {
-        console.log(posData);
-      }, 2000);
+      // setTimeout(() => {
+      //   console.log(posData);
+      // }, 2000);
     },
     (error) => {
       console.log(error);
@@ -18,9 +33,13 @@ function trackUserHandler() {
   );
   // NOTE: this line always executes AFTER the line ('Getting positions...') because it's async and gets passed to the browser, which takes it through event loop and message queue:
   // NOTE: the lag time in setTimeout() is a minimum, it's not a guaranteed time however (e.g. if we have a large for loop that blocks the call stack, etc.).
-  setTimeout(() => {
+  // setTimeout(() => {
+  //   console.log('Timer Done!');
+  // }, 0);
+  // NOTE: we can also use a promise-based approach:
+  setTimer(1000).then(() => {
     console.log('Timer Done!');
-  }, 0);
+  });
   // NOTE: no matter what, this line always gets printed before the other two logs (because of event loop and message queue):
   console.log('Getting position...');
 }
