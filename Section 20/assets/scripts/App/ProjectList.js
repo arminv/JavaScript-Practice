a7d7827884b746b22031179cae44bc693a6ec8ce
arminv <arminv@users.noreply.github.com>
2021-02-17 01:07:26 -0500

    End of 400

--- Section 20/assets/scripts/App/ProjectList.js
+++ Section 20/assets/scripts/App/ProjectList.js
@@ -1,4 +1,7 @@
-class ProjectList {
+import { ProjectItem } from './ProjectItem';
+import { DOMHelper } from '../Utility/DOMHelper.js';
+
+export class ProjectList {
   projects = [];
 
   constructor(type) {
